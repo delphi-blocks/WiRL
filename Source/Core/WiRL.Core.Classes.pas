ff9028f119f4299f4ad9bf074bc1be4f864f11e7
Paolo <paolo@paolorossi.net>
2021-01-15 15:45:17 +0100

    Added TWiRLStringArray type

--- Source/Core/WiRL.Core.Classes.pas
+++ Source/Core/WiRL.Core.Classes.pas
@@ -18,6 +18,17 @@ uses
 type
   EWiRLException = class(Exception);
 
+  { TODO -opaolo -c : Transform it in a record 13/01/2021 17:02:50 }
+  TWiRLStringArray = TArray<string>;
+  TWiRLStringArrayHelper = record helper for TWiRLStringArray
+  public
+    function Length: Integer;
+    function Contains(const AValue: string): Boolean;
+    procedure FromString(const AValue: string);
+    function ToString: string;
+    class function New(const AValue: string): TWiRLStringArray;
+  end;
+
   TNonInterfacedObject = class(TObject, IInterface)
   protected
     function QueryInterface(const IID: TGUID; out Obj): HResult; stdcall;
@@ -158,6 +169,40 @@ end;
 class operator TBearerAuth.Implicit(AAuth: TBearerAuth): string;
 begin
   Result := 'Bearer ' + AAuth.FToken;
+end;
+
+{ TWiRLStringArrayHelper }
+
+function TWiRLStringArrayHelper.Contains(const AValue: string): Boolean;
+var
+  LItem: string;
+begin
+  Result := False;
+  for LItem in Self do
+  begin
+    if SameText(LItem, AValue) then
+      Exit(True);
+  end;
+end;
+
+procedure TWiRLStringArrayHelper.FromString(const AValue: string);
+begin
+  Self := AValue.Split([', ', ',']);
+end;
+
+function TWiRLStringArrayHelper.Length: Integer;
+begin
+  Result := System.Length(Self);
+end;
+
+class function TWiRLStringArrayHelper.New(const AValue: string): TWiRLStringArray;
+begin
+  Result.FromString(AValue);
+end;
+
+function TWiRLStringArrayHelper.ToString: string;
+begin
+  Result := string.Join(', ', Self);
 end;
 
 end.
